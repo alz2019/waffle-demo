0d735394ac2e2f96f4917cd7e9e0ac04f205907a
alz2019 <az2019git@rambler.ru>
2023-09-12 22:56:17 +0300

    Initial commit

---
+++ src/main/java/com/alz2019/waffledemo/WaffleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.alz2019.waffledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaffleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaffleDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alz2019/waffledemo/WaffleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alz2019.waffledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaffleDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
